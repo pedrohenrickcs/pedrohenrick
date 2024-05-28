129407cc68a49ae30d198feda0a686ba22587013
Pedro Henrick <pedro.henrickcs@gmail.com>
2024-05-28 12:21:24 -0300

    fix: 🐛 update next config

--- next.config.js
+++ next.config.js
@@ -1,22 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
-        port: '',
-        pathname: '/u/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.imgur.com',
-        port: '',
-        pathname: '**',
-      },
-    ],
-  },
+  images: {},
 };
 
 module.exports = nextConfig;
