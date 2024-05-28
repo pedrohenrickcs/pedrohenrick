eb9e0eabd2b0ab36bb23107de201c0d22f24f966
Pedro Henrick <pedro.henrickcs@gmail.com>
2024-05-28 16:35:53 -0300

    adjust image

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -5,7 +5,7 @@ const Profile = () => {
     <div className="container mx-auto px-4 p-4 text-center flex justify-around items-center flex-col md:flex-row">
 
        <div className="container mx-auto px-4 p-4 text-center">
-        <Image src="./images/me.jpg" alt="me" width={250} height={128} className="rounded-3xl m-auto"/>
+        <Image src="./me.jpg" alt="me" width={250} height={128} className="rounded-3xl m-auto"/>
         <h1 className="pt-6 text-xl">Pedro Henrick Cavalcante Souza</h1>
         <h2 className="text-base text-tertiary-color">Front end Developer</h2>
       </div>
