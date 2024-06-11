418e81139fd8761796f2b7bb658f9b1ed97a841f
Pedro Henrick <pedro.henrickcs@gmail.com>
2024-06-11 11:11:27 -0300

    style: 💄 mobile

--- src/components/Stack.tsx
+++ src/components/Stack.tsx
@@ -14,11 +14,11 @@ const Stack = ({ title, icons }: StackType) => {
   return (
     <div className="container h-[70vh] mx-auto py-10 flex justify-evenly flex-col md:flex-row">
       <div>
-        <h1 className="text-sm font-medium">{title}</h1>
+        <h1 className="text-sm font-medium text-center md:text-left">{title}</h1>
 
-        <div className="flex flex-row justify-between my-7">
+        <div className="flex flex-row md:justify-between my-7 flex-wrap justify-center">
           {icons.map((item, index) => (
-            <div className="text-center mx-6 first:ml-0" key={index}>
+            <div className="text-center m-6 first:ml-0" key={index}>
               <span className="flex justify-center">
                 <Icon name={item.icon} />
               </span>
